137286c6bca0b8d41007173aef59727994b59ffe
Maksim Manzhai <bender23max@gmail.com>
2019-11-16 23:36:07 +0300

    Edit class cards

--- classes/cards.rb
+++ classes/cards.rb
@@ -1,16 +1,16 @@
 class Cards
-  attr_reader :cards
+  attr_reader :card_deck
 
   def initialize
     nominals_1 = ['A', 'K', 'Q', 'J', '10', '9', '8', '7', '6', '5', '4', '3', '2']
     suits = ['+', '<3', '^', '<>']
     nominals_2 = []
-    suits.each do |nominal|
-      nominals_1.each do |suit|
+    suits.each do |suit|
+      nominals_1.each do |nominal|
         nominals_2 << (nominal + suit)
       end
     end
     points = [11, 10, 10, 10, 10, 9, 8, 7, 6, 5, 4, 3, 2]
-    @cards = Hash[nominals_2.zip(points * 4)]
+    @card_deck = Hash[nominals_2.zip(points * 4)]
   end
 end
