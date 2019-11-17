9e9d7cc6a97a9839024f47d764eb288b5442d796
Maksim Manzhai <bender23max@gmail.com>
2019-11-17 13:36:36 +0300

    Add removal of issued cards from the hash

--- classes/game.rb
+++ classes/game.rb
@@ -2,9 +2,10 @@ class Game
   def initialize
   end
 
-  def dealing(cards, player, dealer)
-    player.hands = cards.to_a.sample(2).to_h
-    dealer.hands = cards.to_a.sample(2).to_h
+  def dealing(cards, user, dealer)
+    user.hands = cards.card_deck.to_a.sample(2).to_h
+    user.hands.each_key { |key| (cards.card_deck).delete(key) }
+    dealer.hands = cards.card_deck.to_a.sample(2).to_h
+    dealer.hands.each_key { |key| (cards.card_deck).delete(key) }
   end
-
 end
