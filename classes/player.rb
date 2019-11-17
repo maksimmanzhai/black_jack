baaf21094c28581c96e4cf26dee5de197ccd14aa
Maksim Manzhai <bender23max@gmail.com>
2019-11-17 13:37:34 +0300

    Adding card points on hand

--- classes/player.rb
+++ classes/player.rb
@@ -1,9 +1,19 @@
 class Player
   attr_accessor :name, :hands, :bank
+  attr_reader :score
 
   def initialize(name)
     @name = name
     @hands = {}
     @bank = 100
+    @score = 0
+  end
+
+  def show_hands
+    puts @hands
+  end
+
+  def scoring
+    @hands.each_value { |value| @score += value }
   end
 end
