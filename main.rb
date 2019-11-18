9abe773b5779268db1ac3714b017acbb8bb3748a
Maksim Manzhai <bender23max@gmail.com>
2019-11-18 22:16:33 +0300

    Add methods

--- main.rb
+++ main.rb
@@ -12,43 +12,50 @@ class Main
   def menu
     puts "Hello. Enter your name"
     user_name = gets.chomp.capitalize
-    user = create_user(user_name)
-    dealer = create_dealer
-    start_game(user, dealer)
-    user.scoring
-    dealer.scoring
-    user.bet
-    dealer.bet
-    puts "#{user.name}: #{user.score}"
-    puts "#{user.show_hands}"
+    @user = create_user(user_name)
+    @dealer = create_dealer
+    game_menu
+  end
+
+  def game_menu
+    @user.hands = {}
+    @dealer.hands = {}
+    start_game
+    @user.scoring
+    @dealer.scoring
+    @user.bet
+    @dealer.bet
+    puts "#{@user.name}: #{@user.score}"
+    puts "#{@user.show_hands}"
     choice = user_play
     case choice
     when 1
-      dealer_play(dealer)
+      dealer_play(@dealer)
     when 2
-      add_card(user)
+      add_card(@user)
     when 3
-      open_cards(user, dealer)
+      open_cards
     else
       puts 'Good bye. Try again'
     end
-    dealer_play(dealer)
-    open_cards(user, dealer)
+    dealer_play(@dealer)
+    open_cards
+    play_again
   end
 
   def create_user(name)
-    user = User.new(name)
+    User.new(name)
   end
 
   def create_dealer
-    dealer = Dealer.new
+    Dealer.new
   end
 
-  def start_game(user, dealer)
+  def start_game
     @cards = Cards.new
     @game = Game.new
-    2.times { @game.add_card(@cards, user) }
-    2.times { @game.add_card(@cards, dealer) }
+    2.times { @game.add_card(@cards, @user) }
+    2.times { @game.add_card(@cards, @dealer) }
   end
 
   def user_play
@@ -60,40 +67,51 @@ class Main
   end
 
   def dealer_play(dealer)
-    @game.add_card(@cards, dealer) if dealer.scoring < 17 && dealer.hands.size <= 3
+    @game.add_card(@cards, @dealer) if @dealer.scoring < 17 && @dealer.hands.size <= 3
   end
 
   def add_card(player)
     @game.add_card(@cards, player) if player.hands.size <= 3
   end
   
-  def open_cards(user, dealer)
-    counting_results(user, dealer)
-    puts "#{user.name} bank: #{user.bank}"
-    puts "#{user.name}: #{user.score}"
-    puts "#{user.show_hands}"
-    puts "#{dealer.name} bank: #{dealer.bank}"
-    puts "#{dealer.name}: #{dealer.score}"
-    puts "#{dealer.show_hands}"
+  def open_cards
+    counting_results
+    puts "#{@user.name} bank: #{@user.bank}"
+    puts "#{@user.name}: #{@user.score}"
+    puts "#{@user.show_hands}"
+    puts "#{@dealer.name} bank: #{@dealer.bank}"
+    puts "#{@dealer.name}: #{@dealer.score}"
+    puts "#{@dealer.show_hands}"
   end
 
-  def counting_results(user, dealer)
-    user.scoring
-    dealer.scoring
-    if user.score == dealer.score
+  def counting_results
+    @user.scoring
+    @dealer.scoring
+    if @user.score == @dealer.score
       result = 'Draw'
-      user.gain(10)
-      dealer.gain(10)
+      @user.gain(10)
+      @dealer.gain(10)
     end
-    if (user.score > dealer.score && user.score < 21) || user.score == 21
-      result = "#{user.name} win"
-      user.gain(20)
+    if (@user.score > @dealer.score && @user.score < 21) || @user.score == 21
+      result = "#{@user.name} win"
+      @user.gain(20)
     end
-    if user.score < dealer.score || user.score > 21
-      result = "#{dealer.name} win"
-      dealer.gain(20)
+    if @user.score < @dealer.score || @user.score > 21
+      result = "#{@dealer.name} win"
+      @dealer.gain(20)
     end
     puts result
+  end
+
+  def play_again
+    puts "Will you play again? (Y/N)"
+    input = gets.chomp
+    case input
+    when 'y'
+      game_menu
+    when 'n'
+      puts 'Good bye. Try again'
+    end
   end
 end
 
