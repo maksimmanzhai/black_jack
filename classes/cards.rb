47933e4dd1b436c61d070229db1d003160a70cda
Maksim Manzhai <bender23max@gmail.com>
2019-11-19 21:45:20 +0300

    Edit main class

--- classes/cards.rb
+++ classes/cards.rb
@@ -3,7 +3,13 @@ class Cards
 
   def initialize
     nominals_1 = ['A', 'K', 'Q', 'J', '10', '9', '8', '7', '6', '5', '4', '3', '2']
-    suits = ['+', '<3', '^', '<>']
+
+    spade = "\u2660"
+    heart = "\u2665"
+    diamond = "\u2666"
+    club = "\u2663"
+
+    suits = [spade, heart, diamond, club]
     nominals_2 = []
     suits.each do |suit|
       nominals_1.each do |nominal|
--- main.rb
+++ main.rb
@@ -1,23 +1,29 @@
+require './classes/interface.rb'
 require './classes/player.rb'
 require './classes/cards.rb'
 require './classes/dealer.rb'
 require './classes/game.rb'
 require './classes/user.rb'
 
+# 2. Заведи отдельный класс Hand - он будет хранить карты игрока, считать 
+# сумму очков
+
+# 3. Класс Cards лучше переработать в Card - он будет представлять карту, 
+# отвечать навопрос "сколько очков стоит карта"
+
+# Масти все-таки лучше сделать юникодом.
+# Убери всю логику из initialize
+# nominals и suits лучше сделать константой в классе Card
 
 class Main
   def initialize
   end
 
-  def menu
-    puts "Hello. Enter your name"
-    user_name = gets.chomp.capitalize
+  def game_menu
+    @interface = Interface.new
+    user_name = @interface.name
     @user = create_user(user_name)
     @dealer = create_dealer
-    game_menu
-  end
-
-  def game_menu
     @user.hands = {}
     @dealer.hands = {}
     start_game
@@ -25,22 +31,22 @@ class Main
     @dealer.scoring
     @user.bet
     @dealer.bet
-    puts "#{@user.name}: #{@user.score}"
-    puts "#{@user.show_hands}"
-    choice = user_play
+    @interface.show_hands(@user)
+    @interface.show_score(@user)
+    choice = @interface.user_play
     case choice
     when 1
       dealer_play(@dealer)
     when 2
       add_card(@user)
     when 3
-      open_cards
+      @interface.open_cards(@user, @dealer)
     else
       puts 'Good bye. Try again'
     end
     dealer_play(@dealer)
-    open_cards
-    play_again
+    @interface.open_cards(@user, @dealer)
+    @interface.play_again
   end
 
   def create_user(name)
@@ -58,30 +64,12 @@ class Main
     2.times { @game.add_card(@cards, @dealer) }
   end
 
-  def user_play
-    puts 'What is your choice?'
-    puts '1 - Skip'
-    puts '2 - Add card'
-    puts '3 - Open cards'
-    choice = gets.chomp.to_i
-  end
-
   def dealer_play(dealer)
     @game.add_card(@cards, @dealer) if @dealer.scoring < 17 && @dealer.hands.size <= 3
   end
 
   def add_card(player)
     @game.add_card(@cards, player) if player.hands.size <= 3
-  end
-  
-  def open_cards
-    counting_results
-    puts "#{@user.name} bank: #{@user.bank}"
-    puts "#{@user.name}: #{@user.score}"
-    puts "#{@user.show_hands}"
-    puts "#{@dealer.name} bank: #{@dealer.bank}"
-    puts "#{@dealer.name}: #{@dealer.score}"
-    puts "#{@dealer.show_hands}"
   end
 
   def counting_results
@@ -100,20 +88,9 @@ class Main
       result = "#{@dealer.name} win"
       @dealer.gain(20)
     end
-    puts result
-  end
-
-  def play_again
-    puts "Will you play again? (Y/N)"
-    input = gets.chomp
-    case input
-    when 'y'
-      game_menu
-    when 'n'
-      puts 'Good bye. Try again'
-    end
+    @interface.show_result(result)
   end
 end
 
-@game = Main.new
-@game.menu
+$main = Main.new
+$main.game_menu
