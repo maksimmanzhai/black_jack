36d501258ea13c93309ace606a80ddda25690ec3
Maksim Manzhai <bender23max@gmail.com>
2019-11-21 10:43:56 +0300

    Editing victory conditions

--- classes/game.rb
+++ classes/game.rb
@@ -8,12 +8,14 @@ class Game
   end
 
   def counting_results(user, dealer)
+    user.hands.scoring
+    dealer.hands.scoring
     if user.hands.score == dealer.hands.score
       result = 'Draw'
       user.gain(10)
       dealer.gain(10)
     end
-    if ((user.hands.score > dealer.hands.score && user.hands.score < 21) || user.hands.score == 21) || dealer.hands.score > 21
+    if (user.hands.score > dealer.hands.score && user.hands.score < 21) || user.hands.score == 21 || dealer.hands.score > 21
       result = "#{user.name} win"
       user.gain(20)
     end
