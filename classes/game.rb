a1361d7223d419d94c1e96d9f80a83a6c58d847e
maksimmanzhai <bender23max@gmail.com>
2019-11-22 21:26:17 +0300

    Edit counting_results

--- classes/game.rb
+++ classes/game.rb
@@ -15,11 +15,11 @@ class Game
       user.gain(10)
       dealer.gain(10)
     end
-    if (user.hands.score > dealer.hands.score && user.hands.score < 21) || user.hands.score == 21 || dealer.hands.score > 21
+    if ((user.hands.score > dealer.hands.score && user.hands.score < 21) || user.hands.score == 21) || dealer.hands.score > 21
       result = "#{user.name} win"
       user.gain(20)
     end
-    if user.hands.score < dealer.hands.score || user.hands.score > 21
+    if (user.hands.score < dealer.hands.score) || user.hands.score > 21
       result = "#{dealer.name} win"
       dealer.gain(20)
     end
