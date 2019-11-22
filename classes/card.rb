bfe7de5109fd62776938271c27acf76e75cb358c
maksimmanzhai <bender23max@gmail.com>
2019-11-23 01:34:20 +0300

    Edit class Card

--- classes/card.rb
+++ classes/card.rb
@@ -1,28 +1,8 @@
 class Card
-  attr_accessor :card_deck
+  attr_accessor :nominal, :suit
 
-  NOMINALS_1 = ['A', 'K', 'Q', 'J', '10', '9', '8', '7', '6', '5', '4', '3', '2']
-
-  SPADES = "\u2660"
-  HEARTS = "\u2665"
-  DIAMONDS = "\u2666"
-  CLUBS = "\u2663"
-
-  SUITS = [SPADES, HEARTS, DIAMONDS, CLUBS]
-
-  POINTS = [11, 10, 10, 10, 10, 9, 8, 7, 6, 5, 4, 3, 2]
-
-  def initialize
-    @card_deck
-  end
-
-  def deck_creation
-    nominals_2 = []
-    SUITS.each do |suit|
-      NOMINALS_1.each do |nominal|
-        nominals_2 << (nominal + suit)
-      end
-    end
-    @card_deck = Hash[nominals_2.zip(POINTS * 4)]
+  def initialize (nominal, suit)
+    @nominal = nominal
+    @suit = suit
   end
 end
