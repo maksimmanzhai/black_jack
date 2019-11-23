914516d3aa6f83d54cbef6c86d395ac6b47200c1
maksimmanzhai <bender23max@gmail.com>
2019-11-24 01:08:18 +0300

    Replace constants

--- classes/deck.rb
+++ classes/deck.rb
@@ -4,12 +4,6 @@
 class Deck
   attr_accessor :deck_of_cards
 
-  NOMINALS = %w[A K Q J 10 9 8 7 6 5 4 3 2].freeze
-
-  SUITS = ["\u2660", "\u2665", "\u2666", "\u2663"].freeze
-
-  POINTS = [11, 10, 10, 10, 10, 9, 8, 7, 6, 5, 4, 3, 2].freeze
-
   def initialize
     @deck_of_cards = {}
   end
