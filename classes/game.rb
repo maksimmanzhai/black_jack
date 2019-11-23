66ace5abb4ab2a5c5b6475cd5e80c913b0589237
maksimmanzhai <bender23max@gmail.com>
2019-11-24 01:09:08 +0300

    Adding methods for comparison conditions

--- classes/game.rb
+++ classes/game.rb
@@ -2,22 +2,44 @@
 
 # class for creating rule of Game
 class Game
-  def counting_results(user, dealer)
-    user_score = user.hands.scoring
-    dealer_score = dealer.hands.scoring
-    if user_score == dealer_score
-      result = 'Draw'
-      user.gain(10)
-      dealer.gain(10)
+  def counting_results(user, dealer, interface)
+    @user = user
+    @dealer = dealer
+    if draw?(user.hands.scoring, dealer.hands.scoring)
+      draw
+    elsif user_won?(user.hands.scoring, dealer.hands.scoring)
+      user_victory
+    elsif dealer_won?(user.hands.scoring, dealer.hands.scoring)
+      dealer_victory
     end
-    if ((user_score > dealer_score && user_score < 21) || user_score == 21) || dealer_score > 21
-      result = "#{user.name} win"
-      user.gain(20)
-    end
-    if (user_score < dealer_score) || user_score > 21
-      result = "#{dealer.name} win"
-      dealer.gain(20)
-    end
-    $interface.show_result(result)
+    interface.show_result(@result)
+  end
+
+  def draw?(user, dealer)
+    user == dealer
+  end
+
+  def draw
+    @user.gain(10)
+    @dealer.gain(10)
+    @result = 'Draw'
+  end
+
+  def user_won?(user, dealer)
+    ((user > dealer && user < 21) || user == 21) || dealer > 21
+  end
+
+  def user_victory
+    @user.gain(20)
+    @result = "#{@user.name} win"
+  end
+
+  def dealer_won?(user, dealer)
+    (user < dealer) || user > 21
+  end
+
+  def dealer_victory
+    @dealer.gain(20)
+    @result = "#{@dealer.name} win"
   end
 end
